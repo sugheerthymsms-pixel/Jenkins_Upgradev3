a57e3b47469c6653d984eecd85a4ae3ce26d98c6
sugheerthymsms-pixel <sugheerthymsms@gmail.com>
2026-06-15 17:34:14 +0530

    Update Greeter.java
    change1

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Hello and welcome to the DevOps Master Class   !!!");
+	  System.out.println("Hello  !!!");
   }
 	
 }
